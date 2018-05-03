82a312b9f47159fdc12c80519e839b3dac536338
tomerdayan168 <tomerdayan168@gmail.com>
2018-05-03 04:00:54 -0700

    initial gui commit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IPAddress serverIp;
+        private int serverPort;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Client();
+
+            VM vmModel = new VM(serverPort, serverIp);
+        }
+
+        private void Client()
+        {
+            settings.Content = "gfjfhd";
+            try
+            {
+                IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+              //  IPEndPoint ipLocalEndPoint = new IPEndPoint(ipAddress, 8888);
+                TcpClient client = new TcpClient();
+                client.Connect("127.0.0.1", 8888);
+
+                NetworkStream ns = client.GetStream();
+
+                byte[] bytes = new byte[1024];
+                int bytesRead = ns.Read(bytes, 0, bytes.Length);
+
+                Console.WriteLine(Encoding.ASCII.GetString(bytes, 0, bytesRead));
+
+                serverPort = 9000;
+                serverIp = ipAddress;
+                client.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
+        }
+    }
+}
---
+++ WpfApp1/VM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    //this is the view model
+    class VM
+    {
+        private int port;
+        private IPAddress ip;
+        private MainWindow mainWindow;
+
+        public VM(int serverPort , IPAddress serverIP, MainWindow mainWindow) {
+
+            port = serverPort;
+            ip = serverIP;
+
+        }
+
+        public string ReadSocket()
+        {
+            return "s";
+        }
+
+        public string WriteSocket()
+        {
+            return "S";
+        }
+    }
+}
